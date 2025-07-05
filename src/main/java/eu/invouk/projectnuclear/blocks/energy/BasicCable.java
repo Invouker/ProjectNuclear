9b8a936eaa856dbc29e63ff921c9c994cb4d231f
Invouk <invouk@gmail.com>
2025-07-05 11:37:33 +0200

    Cleaning the method.

--- src/main/java/eu/invouk/projectnuclear/blocks/energy/BasicCable.java
+++ src/main/java/eu/invouk/projectnuclear/blocks/energy/BasicCable.java
@@ -12,12 +12,8 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
 import org.jetbrains.annotations.Nullable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class BasicCable extends BaseEntityBlock {
-
-    private static final Logger log = LoggerFactory.getLogger(BasicCable.class);
 
     public BasicCable(Properties properties) {
         super(properties);
@@ -32,59 +28,6 @@ public class BasicCable extends BaseEntityBlock {
     public @Nullable BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
         return new BasicCableTile(blockPos, blockState);
     }
-/*
-    @Override
-    protected InteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
-        if(hand != InteractionHand.MAIN_HAND) return InteractionResult.FAIL;
-        if (!level.isClientSide && player.isShiftKeyDown()) {
-
-            player.displayClientMessage(Component.literal("---------------------------------------"), false);
-            player.displayClientMessage(Component.empty(),false);
-
-            BlockEntity blockEntity = level.getBlockEntity(pos);
-            IEnergyNode energyNode = (IEnergyNode) blockEntity;
-            EnergyNet net = energyNode.getEnergyNet();
-            if (net == null) {
-                player.displayClientMessage(Component.literal("Tento blok nie je pripojený k EnergyNetu."), false);
-                return InteractionResult.SUCCESS;
-            }
-            player.displayClientMessage(Component.literal("=== EnergyNet info ==="), false);
-            player.displayClientMessage(Component.literal("== ## Počet uzlov: " + net.getAllNodes().size()), false);
-            player.displayClientMessage(Component.literal("== ## Sieť validná: " + net.isValid()), false);
-            player.displayClientMessage(Component.empty(),false);
-            for (IEnergyNode node : net.getAllNodes()) {
-                String info = String.format("== ## %s @ ", node.getClass().getSimpleName());
-                player.displayClientMessage(Component.literal(info),false);
-                if(node instanceof BlockEntity bufferTile) {
-                    player.displayClientMessage(Component.literal("## Position @ - X:" + bufferTile.getBlockPos().getX() + ", Y: " + bufferTile.getBlockPos().getY() + ", Z: " + bufferTile.getBlockPos().getZ()),false);
-                }
-                if(node instanceof IEnergyProducer producer) {
-                    player.displayClientMessage(Component.literal("-- Stored energy: " + producer.getEnergyStorage().getEnergyStored()), false);
-                    player.displayClientMessage(Component.literal("-- Max energy stored: " + producer.getEnergyStorage().getMaxEnergyStored()), false);
-                    player.displayClientMessage(Component.empty(),false);
-                    if(producer instanceof CoalGeneratorTile bufferTile) {
-                        player.displayClientMessage(Component.empty(),false);
-                        player.displayClientMessage(Component.literal("-- Voltage: " + bufferTile.getVoltage()),false);
-                    }
-                } else if (node instanceof IEnergyConsumer consumer){
-                    player.displayClientMessage(Component.literal(info),false);
-                    player.displayClientMessage(Component.literal("-- Priority: " + consumer.getPriority()), false);
-                    player.displayClientMessage(Component.literal("-- Voltage: " + consumer.getVoltage()), false);
-                    player.displayClientMessage(Component.literal("-- IsAlive: " + consumer.isAlive()), false);
-                    player.displayClientMessage(Component.empty(),false);
-                } else if (node instanceof IEnergyCable cable){
-                    player.displayClientMessage(Component.literal("-- Priority: " + cable.getVoltageRating()),false);
-                    player.displayClientMessage(Component.literal("-- Capacity: " + cable.getCapacity()),false);
-                    player.displayClientMessage(Component.empty(),false);
-                }
-            }
-            player.displayClientMessage(Component.empty(),false);
-            player.displayClientMessage(Component.literal("---------------------------------------"), false);
-            return InteractionResult.SUCCESS;
-        }
-        return InteractionResult.PASS;
-    }
-*/
 
     @Override
     protected void onPlace(BlockState state, Level level, BlockPos pos, BlockState oldState, boolean movedByPiston) {
@@ -103,6 +46,5 @@ public class BasicCable extends BaseEntityBlock {
         }
         return super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid);
     }
-
 
 }
